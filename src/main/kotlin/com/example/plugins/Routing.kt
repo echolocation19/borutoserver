09ce2f939aee69038357def031370b8ae40491e0
echolocation19 <sakur2420@mail.ru>
2022-05-18 17:21:20 +0300

    add limit parameter

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,20 +1,17 @@
 package com.example.plugins
 
-import com.example.routes.getAllHeroes
+import com.example.routes.getAllHeroesAlternative
 import com.example.routes.root
 import com.example.routes.searchHeroes
-import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
 import io.ktor.http.content.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
 
 fun Application.configureRouting() {
 
     routing {
         root()
-        getAllHeroes()
+        getAllHeroesAlternative()
         searchHeroes()
         static("/images") {
             resources("images")
